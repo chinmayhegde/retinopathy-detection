2e521cd81967d731e259b76847e7e64d73500a2d
Brian Shannan <brianshannan@gmail.com>
2016-02-08 11:13:44 -0600

    basic working handler

--- digital_recognition/retinopathy_recognition/main.py
+++ digital_recognition/retinopathy_recognition/main.py
@@ -1,11 +1,11 @@
 
 import cv2
-# import numpy
+import numpy
 import csv
 
 
 if __name__ == '__main__':
-    # Get image names and associate
+    # Get image names and classifications
     names = {i: [] for i in range(5)}
     with open('subsetcsv.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size,
                             num_bins)
 
+    # Load all images into memory for now
     images = {i: [] for i in range(5)}
     for classification, image_names in names.iteritems():
         for image_name in image_names:
@@ -28,44 +29,53 @@ if __name__ == '__main__':
             image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
             image = cv2.resize(image, (2592, 3888))
             image = hog.compute(image)
-            print image.shape
             images[classification].append(image)
 
-    for classiction in images.iterkeys():
-        print len(images[classification])
+    # Partition images into test and train sets
+    train_ratio = 0.75
+    train_labels = []
+    train_data = []
+    test_labels = []
+    test_data = []
 
+    for classification, image_list in images.iteritems():
+        train_num = int(len(image_list) * train_ratio)
+        train_labels.extend([classification for _ in range(train_num)])
+        train_data.extend(image_list[:train_num])
+        test_labels.extend([classification for _ in
+                            range(len(image_list) - train_num)])
+        test_data.extend(image_list[train_num:])
 
-"""
-def get_default_image_data():
-    img = cv2.imread('digits.png', cv2.IMREAD_GRAYSCALE)
+    train_labels = numpy.array(train_labels)
+    train_data = numpy.array(train_data)
+    test_labels = numpy.array(test_labels)
+    test_data = numpy.array(test_data)
 
-    # We know the sub-images are 20x20, and there are 50x100 sub-images in the
-    # larger image. Works for this one, might need to make better in future.
-    cells_list = [numpy.hsplit(row, 100) for row in numpy.vsplit(img, 50)]
-    cells = numpy.array(cells_list)  # 50x100 array of 20x20 arrays
+    # Classify, move these to other classes
+    # KNN
+    knn = cv2.KNearest()
+    knn.train(train_data, train_labels)
+    _, knn_result, _, _ = knn.find_nearest(test_data, k=3)
 
-    # Divide dataset in half, half to train and half to test
-    # Make each half a 2D array, the images flattened to a single array
-    # and each entry in the first array is an image.
-    train_data = cells[:, :50].reshape((-1, 400)).astype(numpy.float32)
-    test_data = cells[:, 50:100].reshape((-1, 400)).astype(numpy.float32)
+    knn_correct = 0
+    for i in range(knn_result.size):
+        if knn_result[i] == test_labels[i]:
+            knn_correct += 1
+    print 'knn:', knn_correct, 1.0 * knn_correct / knn_result.size
 
-    # Form expected, it will be the same for each piece as we divided in half
-    expected = numpy.repeat(numpy.arange(10), 250)[:, numpy.newaxis]
+    # SVM
+    svm_params = {
+        'kernel_type': cv2.SVM_LINEAR,
+        'svm_type': cv2.SVM_C_SVC,
+    }
 
-    return train_data, test_data, expected
+    svm = cv2.SVM()
+    svm.train(train_data, train_labels, params=svm_params)
+    svm_result = svm.predict_all(test_data)
 
-
-def get_default_image_transformed(bin_n=16, size=20):
-    img = cv2.imread('digits.png', 0)
-    cells = [numpy.hsplit(row, 100) for row in numpy.vsplit(img, 50)]
-
-    transformed = transform(cells)
-    train_data = numpy.float32([t[50:] for t in transformed])
-    train_data = train_data.reshape(-1, bin_n * 4)
-    test_data = numpy.float32([t[:50] for t in transformed])
-    test_data = test_data.reshape(-1, bin_n * 4)
-    expected = numpy.repeat(numpy.arange(10), 250)[:, numpy.newaxis]
-    return train_data, test_data, expected
-"""
+    svm_correct = 0
+    for i in range(svm_result.size):
+        if svm_result[i] == test_labels[i]:
+            svm_correct += 1
+    print 'svm:', svm_correct, 1.0 * svm_correct / svm_result.size
 
