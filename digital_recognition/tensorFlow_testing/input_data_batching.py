e35c94171d8352c1e7df90fda2918360b5a5e3c1
root <root@sdmay1628-2.ece.iastate.edu>
2016-03-26 01:29:43 -0500

    Updated TF stuff

--- digital_recognition/tensorFlow_testing/input_data_batching.py
+++ digital_recognition/tensorFlow_testing/input_data_batching.py
@@ -13,12 +13,17 @@ import tensorflow as tf
 
 class DataSet(object):
 
-  def __init__(self, name_label_association, fake_data=False, one_hot=False,
+  def __init__(self, is_necessary, name_label_association, fake_data=False, one_hot=False,
                dtype=tf.float32):
 
     dtype = tf.as_dtype(dtype).base_dtype
 
-    self._images = [] #Images loaded in batching
+    if ( is_necessary ):
+      print "Loading images for test/validation."
+      self._images = get_test_images(name_label_association)
+    else:
+      self._images = [] #Images loaded in batching
+
     self._labels = name_label_association 
     self._epochs_completed = 0
     self._index_in_epoch = 0
@@ -91,11 +96,11 @@ def read_data_sets(train_dir, dtype=tf.float32):
     pass
   data_sets = DataSets()
 
-  name_label_association = read_labels(train_dir)
+  name_label_association = read_labels()
 
-  TRAIN_SIZE = (len(name_label_association) * 3 ) / 4 #3/4 train
-  TEST_SIZE = len(name_label_association) / 8 #1/8 test
-  VALIDATION_SIZE = len(name_label_association) / 8 #1/8 validation
+  TRAIN_SIZE = (len(name_label_association) * 14 ) / 16 #3/4 train
+  TEST_SIZE = len(name_label_association) / 16 #1/8 test
+  VALIDATION_SIZE = len(name_label_association) / 16 #1/8 validation
 
   train_label_image_association = {}
   test_label_image_association = {}
@@ -112,38 +117,65 @@ def read_data_sets(train_dir, dtype=tf.float32):
 
     index = index + 1
 
-  data_sets.train = DataSet(train_label_image_association, dtype=dtype)
-  data_sets.test = DataSet(test_label_image_association, dtype=dtype)
-  data_sets.validation = DataSet(validation_label_image_association, dtype=dtype)
+  data_sets.train = DataSet(False, train_label_image_association, dtype=dtype)
+  data_sets.test = DataSet(True, test_label_image_association, dtype=dtype)
+  data_sets.validation = DataSet(True, validation_label_image_association, dtype=dtype)
   data_sets._num_examples = len(name_label_association)
 
   return data_sets
 
+def get_test_images(name2label, dtype=tf.float32):
+  label_image_association_to_load = {}
+
+  for key, value in name2label.items():
+    label_image_association_to_load[key] = value
+
+  images , labels = read_given_images_and_labels(label_image_association_to_load)
+
+  #Reshape images/labels
+  images = images.reshape(images.shape[0], images.shape[1] * images.shape[2])
+
+  images = images.astype(numpy.float32)
+  images = numpy.multiply(images, 1.0 / 255.0)
+
+  return images
+
+
 #Used to read a given subset of images
 def read_given_images_and_labels(name2label):
-  images = labels = []
+  images = []
+  labels = []
 
-  for label , image in name2label:
-    image = cv2.imread(os.path.join(directory, image), cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(image (512, 512))
+  index = 0
+
+  for image , label in name2label.items():
+    image = cv2.imread(os.path.join('/root/project/train' , image), cv2.IMREAD_GRAYSCALE)
+    
+    image = cv2.resize(image, (512, 512))
 
     images.append(image)
 
+    if (index % 250) == 0:
+      print "Loaded image", index
+
     #One-Hot vector
-    label = numpy.zeros(5)
-    label[name2label[image_name.split('.')[0]]] = 1
-    labels.append(label)
+    newLabel = numpy.zeros(5)
+    newLabel[int(label) - 1] = float(1)
+
+    labels.append(newLabel)
+
+    index = index + 1
 
   return np.asarray(images), np.asarray(labels)
 
-def read_labels(train_dir):
+def read_labels():
   # Get image names and classifications
   imageLabelAssociation = {}
 
   with open('../../../trainLabels.csv', 'r') as csvfile:
       reader = csv.reader(csvfile)
       for row in reader:
-        imageLabelAssociation[row[0]] = train_dir + '/' + row[1] #Label = imagepath
+        imageLabelAssociation[row[0] + '.jpeg'] = row[1] #Label = imagepath
 
   return imageLabelAssociation
 
