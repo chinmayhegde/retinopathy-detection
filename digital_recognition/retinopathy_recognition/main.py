88536a1e7ba80868b03116e9f0467cbf5b151f1b
Brian Shannan <brianshannan@gmail.com>
2016-03-27 12:18:41 -0500

    fixed crop in the file, added to svm

--- digital_recognition/retinopathy_recognition/main.py
+++ digital_recognition/retinopathy_recognition/main.py
@@ -11,19 +11,19 @@ def get_batch(classifier, image_names, idx, batch_size):
     images = {}
     for image_name in image_names[idx:idx + batch_size]:
         image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
-        image = classifier.preprocess_image(image)
+        image, ok = classifier.preprocess_image(image)
+        if not ok:
+            print 'preprocess failed, skipping image'
+            continue
         images[image_name] = image
     return images
 
-if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print 'Usage: python main.py <classifier> <csv file> <image folder>'
-        sys.exit(1)
 
+def run(classifier, csv_filename, image_folder):
     # Get image names and classifications
     names = []
     name_to_class = {}
-    with open(sys.argv[2], 'r') as csvfile:
+    with open(csv_filename, 'r') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             # skip the first row
@@ -32,18 +32,10 @@ if __name__ == '__main__':
             # uncomment this to get only two categories
             # if int(row[1]) in [1, 2, 3]:
             #     continue
-            image_name = sys.argv[3] + '/' + row[0] + '.jpeg'
+            image_name = image_folder + '/' + row[0] + '.jpeg'
             names.append(image_name)
             name_to_class[image_name] = int(row[1])
     random.shuffle(names)
-
-    classifier = None
-    # Add more casses for other classifiers
-    if sys.argv[1] == 'svm':
-        classifier = SVMBatchClassifier([0, 1, 2, 3, 4])
-    else:
-        print 'Invalid classifier:', sys.argv[1]
-        sys.exit(1)
 
     batch_size = 200
     # TODO check this
@@ -57,14 +49,14 @@ if __name__ == '__main__':
         for image_name, image in image_map.iteritems():
             images.append(image)
             image_classes.append(name_to_class[image_name])
-        print 'train:', idx
+        # print 'train:', idx
 
         train_labels = numpy.array(image_classes)
         train_data = numpy.array(images)
         classifier.train_batch(train_labels, train_data)
 
-    svm_correct = 0
-    svm_total = 0
+    correct = 0
+    total = 0
     for idx in range(train_num, len(names), batch_size):
         this_batch_size = min(len(names) - idx, batch_size)
         image_map = get_batch(classifier, names, idx, this_batch_size)
@@ -73,16 +65,33 @@ if __name__ == '__main__':
         for image_name, image in image_map.iteritems():
             images.append(image)
             image_classes.append(name_to_class[image_name])
-        print 'test:', idx
+        # print 'test:', idx
 
         test_labels = numpy.array(image_classes)
         test_data = numpy.array(images)
 
-        svm_result = classifier.predict(test_data)
-        svm_total += svm_result.size
-        for i in range(svm_result.size):
-            if svm_result[i] == test_labels[i]:
-                svm_correct += 1
+        result = classifier.predict(test_data)
+        total += result.size
+        for i in range(result.size):
+            if result[i] == test_labels[i]:
+                correct += 1
 
-    print 'svm:', svm_correct, 1.0 * svm_correct / svm_total
+    print 'svm:', correct, 1.0 * correct / total
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print 'Usage: python main.py <classifier> <csv file> <image folder>'
+        sys.exit(1)
+
+    classifier = None
+    # Add more casses for other classifiers
+    if sys.argv[1] == 'svm':
+        classifier = SVMBatchClassifier([0, 1, 2, 3, 4])
+    else:
+        print 'Invalid classifier:', sys.argv[1]
+        sys.exit(1)
+
+    for _ in range(10):
+        run(classifier, sys.argv[2], sys.argv[3])
 
--- digital_recognition/retinopathy_recognition/svm_classifier.py
+++ digital_recognition/retinopathy_recognition/svm_classifier.py
@@ -2,6 +2,7 @@ import cv2
 from sklearn import linear_model
 
 from classifier import BatchClassifier
+from cnn import crop
 
 
 class SVMBatchClassifier(BatchClassifier):
@@ -14,9 +15,12 @@ class SVMBatchClassifier(BatchClassifier):
 
     def preprocess_image(self, image):
         # median image size is (2592, 3888)
-        image = cv2.resize(image, (778, 518))
+        # image = cv2.resize(image, (778, 518))
+        image, ok = crop.crop_img(image)
+        if not ok:
+            return None, ok
         image = self.hog.compute(image)
-        return image.flatten()
+        return image.flatten(), True
 
     def train_batch(self, train_labels, train_data):
         self.classifier.partial_fit(train_data, train_labels,
